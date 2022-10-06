454fe911d1a5e9899e0326ce060074987ee6e05e
Dilshad-MD <113435059+Dilshad-MD@users.noreply.github.com>
2022-10-06 19:46:49 +0530

    example
    js

---
+++ pravtic/ifelse.js
@@ -0,0 +1,27 @@
+// num = "kiran";
+
+// if(num%2 == 0){
+// console.log(num, "is even");
+// }else{
+//     console.log(num, "is odd");
+// }
+
+// if (isNaN(num)) {
+//   console.log(num, "is string");
+// } else if (num % 2 == 0) {
+//   console.log(num, "is even");
+// } else {
+//   console.log(num, "is odd");
+// }
+
+// let b = 10;
+
+// console.log(~10);
+
+// let a = -10;
+
+// console.log(~a);
+
+let a = -10;
+let b = 10;
+console.log(("" & a) | b);
---
+++ pravtic/style.js
@@ -0,0 +1,121 @@
+// let arr= [1,2,3,4];
+
+// console.log(arr.length)
+
+// if(arr.length) {
+//     console.log("Hi Hi");
+// } else {
+//     console.log("Bye Byes");}
+
+// '', 0, false, undefined, null
+
+// var x = 4;
+// var y = 2 ;
+
+
+
+// //  console.log(--x + y);
+
+//  const age  = 10;
+      
+
+
+//  console.log(age)
+
+
+// const arr = [10, 15, 20];
+// const target = 15;
+// isTrue = false;
+// for(let i = 0; i < arr.length; i++) {
+//    if(arr[i]===target){
+//     console.log(i)
+//     isTrue= true
+//     break
+
+//    }
+// }
+// if(!isTrue){
+//    console.log(1)
+// }
+    
+
+//WAP to find the target index or return -1
+// const arr = [10, 15, 20];
+// const target=15;
+//  sum = 0;
+// for(let i = 0; i < arr.length; i++) {
+
+//   sum=arr[i]+sum
+
+
+
+
+// }
+// console.log("final sum"+sum);
+    
+// console.log("avg is"+(sum/arr.length))
+
+
+// const arr = [10, 15, 20, 45];
+
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+     
+//     sum=arr[i]+sum
+
+// }
+// console.log("final sum"+sum);
+// console.log("avg is"+(sum/arr.length));
+
+// const arr = [20, 10, 50, 99];
+// let sum = 0;
+// for(let i=0; i < arr.length; i++){
+     
+//    sum=arr[i]+sum
+
+// }
+// console.log("final sum"+sum);
+// console.log("avg is"+(sum/arr.length));
+
+
+// const arr = [10, 20, 30, 40, 60, 50, 5, 4, 99];
+// let sum = 0;
+// for(let i = 0; i <arr.length; i++){
+
+//     sum=arr[i]+sum
+// }
+
+// console.log("final sum"+sum);
+// console.log("avg is"+(sum % arr.length));
+
+// let a = 9199129309;
+// console.log(a.toString(2));
+
+// 101
+// 101
+// ----
+//    1010
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
