418b8966cc348f00fa719d08ca3f424daff458c8
Dilshad-MD <113435059+Dilshad-MD@users.noreply.github.com>
2022-10-13 17:31:26 +0530

    if
    else

---
+++ if.js
@@ -0,0 +1,8 @@
+let age = 20;
+
+if (age > 18) {
+  age = "you can drive";
+} else {
+  age = "you cannot drive  ";
+}
+console.log(age);
---
+++ tarique.js
@@ -0,0 +1,44 @@
+// console.log("I am first line 1");
+// if (false) {
+//   console.log("Hell world");
+//   console.log("Hell world");
+//   console.log("Hell world");
+//   console.log("Hell world");
+// } else {
+//   console.log("Else block");
+// }
+
+// let age = 0;
+
+// while (age < 11) {
+//   if (age < 1) {
+//     console.log("Baby can't walk");
+//   } else if (age < 3) {
+//     console.log("you can speak little bit");
+//   } else if (age < 5) {
+//     console.log("You can take admission in school");
+//   } else if (age < 10) {
+//     console.log("You can start reading");
+//   } else {
+//     console.log("YOu have enough age to do what ever you want");
+//   }
+
+//   age++;
+// }
+
+// console.log(" I am outside of while loop");
+
+let age = 0;
+
+while (age < 18) {
+  if (age < 10) {
+    console.log("you are child");
+  } else if (age < 12) {
+    console.log("you are under age ");
+  } else if (age < 17) {
+    console.log("you can learning to drive");
+  } else {
+    console.log("you can drive");
+  }
+  age++;
+}
